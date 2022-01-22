cf87f2d09c573339edf38a37f57c28fd3ed89d96
AlexandreDresch <z3r0pc@protonmail.com>
2022-01-22 15:05:38 -0300

    Commit inicial

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View,
+     Text,
+      Image,
+       StatusBar,
+    } from 'react-native';
+
+import { ButtonIcon } from "../../components/Buttonicon";
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from "./styles";
+
+export function SignIn() {
+    return(
+        <View style={styles.container}>
+            <StatusBar 
+            barStyle= 'light-content'
+            backgroundColor= 'transparent'
+            translucent
+            />
+            <Image 
+            source={IllustrationImg} 
+            style={styles.image} 
+            resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    your games {`\n`}
+                    easily
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Create groups to play your favorite {`\n`}
+                    games with your friends
+                </Text>
+
+                <ButtonIcon />
+            </View>
+
+        </View>
+    )
+};
