a18406e15fe5f91fb57fc5f3cfff0b3e63cf6bb7
AlexandreDresch <z3r0pc@protonmail.com>
2022-01-27 21:38:08 -0300

    Home building

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
     return(
         <View style={styles.container}>
+
+            <Avatar urlImage="https://github.com/AlexandreDresch.png"/>
+
             <View style={styles.user}>
                 <Text style={styles.greeting}>
                     Hello, 
                 </Text>
 
                 <Text style={styles.username}>
-
+                    Alexandre
                 </Text>
+                
             </View>
-
-            <Text>  // 49
+                
+            <Text style={styles.message}> 
                 Today is victory day. 
             </Text>
         </View>
