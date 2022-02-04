2d3a74a662176a8f80a5bfac18e19097402729f8
AlexandreDresch <z3r0pc@protonmail.com>
2022-02-04 20:59:21 -0300

    Update Routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -9,11 +9,16 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AuthRoutes() {
     return (
-        <Navigator>
+        <Navigator
+        screenOptions={{
+            headerShown: false,
+            
+          }}
+          
+        >
             <Screen
             name="SignIn"
             component={SignIn}
-            
             />
             <Screen
             name="Home"
