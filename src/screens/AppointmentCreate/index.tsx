b4064de62e700faffbad92555d997c98968cc80b
Alexandre <73963734+AlexandreDresch@users.noreply.github.com>
2022-02-19 20:10:56 -0300

    Schedule screen update

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState} from "react";
 import { Feather } from '@expo/vector-icons';
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 
 import { theme } from "../../global/styles/theme";
 import { styles } from "./styles"; 
@@ -9,51 +9,110 @@ import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 import { CategorySelect } from "../../components/CategorySelect";
 import { GuildIcon } from "../../components/GuildIcon";
+import { SmallInput } from "../../components/SmallInput";
+import { TextArea } from "../../components/TextArea";
 
 
 export function AppointmentCreate() {
     const [category, setCategory] = useState('') 
     return(
-        <Background>
-            <Header
-                title="Schedule match"                 
-            />
+        <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height' }
+        style={styles.container}        
+        >
+            <ScrollView>
+            
+                <Header
+                    title="Schedule match"                 
+                />
 
-            <Text style={styles.label}>
-                Category
-            </Text>
+                <Text style={[styles.label, { marginLeft: 24, marginTop: 36, marginBottom: 18}]}>
+                    Category
+                </Text>
 
-            <CategorySelect 
-                hasCheckBox
-                setCategory={setCategory}
-                categorySelected={category}
-            />
+                <CategorySelect 
+                    hasCheckBox
+                    setCategory={setCategory}
+                    categorySelected={category}
+                />
 
-            <View style={styles.form}>
-                <TouchableOpacity>
-                    <View style={styles.select}>
-                        {
-                            //<View 
-                               // style={styles.image}
-                            ///>
-                            <GuildIcon />
-                        }
+                <View style={styles.form}>
+                    <TouchableOpacity>
+                        <View style={styles.select}>
+                            {
+                                /*<View 
+                                    style={styles.image}
+                                /> */
+                                <GuildIcon />
+                            }
 
-                        <View style={styles.selectBody}>
+                            <View style={styles.selectBody}>
+                                <Text style={styles.label}>
+                                    Select a server
+                                </Text>
+                            </View>
+
+                            <Feather 
+                                name="chevron-right"
+                                color={theme.colors.heading}
+                                size={18}
+                            />
+
+                        </View>
+                    </TouchableOpacity>
+
+                    <View style={styles.field}>
+                        <View>
                             <Text style={styles.label}>
-                                Select a server
-                            </Text>
+                            Month and day 
+                            </Text>   
+
+                            <View style={styles.column}>
+                                <SmallInput maxLength={2}/>
+                                <Text style={styles.divider}>
+                                    /
+                                </Text>
+                                <SmallInput maxLength={2}/>
+                            </View>                    
                         </View>
 
-                        <Feather 
-                            name="chevron-right"
-                            color={theme.colors.heading}
-                            size={18}
-                        />
+                        <View>
+                            <Text style={styles.label}>
+                            Hour and minute 
+                            </Text>   
 
+                            <View style={styles.column}>
+                                <SmallInput maxLength={2}/>
+                                <Text style={styles.divider}>
+                                    :
+                                </Text>
+                                <SmallInput maxLength={2}/>
+                            </View>                    
+                        </View>               
                     </View>
-                </TouchableOpacity>
-            </View>
-        </Background>
+                    <View style={[styles.field, {marginBottom: 12}]}>
+                        <Text style={styles.label}>
+                            Description
+                        </Text>
+
+                        <Text style={styles.characterLimit}>
+                            Max 100 characters
+                        </Text>
+                    </View> 
+                    <TextArea 
+                        multiline
+                        maxLength={100}
+                        numberOfLines={5}
+                        autoCorrect={false}
+                    />
+
+                    <View>
+                        
+                    </View>
+                </View>
+
+            </ScrollView>
+            
+        </KeyboardAvoidingView>
     )
 }
